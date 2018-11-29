950f11e7d02fedc3119e1f7896823eb8aa8bc331
ClaireProvot <provot.claire@gmail.com>
2018-11-29 21:46:46 +0800

    remove old folder

--- Pokedex-API/models/types.js
+++
@@ -1,9 +0,0 @@
-// Creation of the model "Types" in DB
-module.exports = (sequelize, Datatypes) => {
-    let Types = sequelize.define('Types', {
-        cname: Datatypes.CHAR(45),
-        ename: Datatypes.CHAR(45),
-        jname: Datatypes.CHAR(45)
-    }, {});
-    return Types;
-};
